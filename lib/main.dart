797a95bfe6e1a627e949761e2470a4922ff59a9e
siddharthsutar <ssiddharthsutar@gmail.com>
2023-09-19 23:47:57 +0530

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:jalseva/screens/SignupScreen/signup_screen_controller.dart';
+import 'package:jalseva/screens/loginScreen/login_screen_controller.dart';
+import 'package:jalseva/screens/mainScreen/main_screen_controller.dart';
+import 'package:jalseva/screens/postScreen/post_screen_controller.dart';
 import 'package:jalseva/screens/splashScreen/splash_screen.dart';
 
 void main() => runApp(MyApp());
@@ -10,12 +14,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/splash': (context) => SplashScreen(),
+        "/":(context) =>  MainScreen(),
+        "/Post": (context) => PostScreen(),
+        "/Login": (context) => LoginScreen(),
+        "/Signup": (context) => SignupScreen(),
+      },
       title: 'Jalseva',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: SplashScreen()
+      initialRoute: '/splash'
     );
   }
 }
