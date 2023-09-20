3189949f138bce9f54f3c4df05b938840ff6a6bb
siddharthsutar <ssiddharthsutar@gmail.com>
2023-09-20 22:38:29 +0530

    cleaned pubspec

---
+++ lib/constants/constants.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter/material.dart';
+
+// const HeadingTextStyle = TextStyle(
+//   fontFamily: 'Duvall',
+//   fontSize: 136.968,
+//   fontWeight: FontWeight.w400,
+//   fontStyle: FontStyle.normal,
+//   letterSpacing: 1.37,
+//   height: 1.5,
+//   color: Colors.white,
+// );
--- lib/screens/splashScreen/splash_screen.dart
+++ lib/screens/splashScreen/splash_screen.dart
@@ -26,7 +26,7 @@ class _SplashScreenState extends State<SplashScreen> {
         width: double.infinity,
         child: Column(
           children: [
-
+            
           ],
         ),
       ),
