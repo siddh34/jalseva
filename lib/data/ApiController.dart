fa68e8aca1895a6467156da84fbe39670ea0fc09
siddharthsutar <ssiddharthsutar@gmail.com>
2023-09-20 23:37:46 +0530

    feat: done api handler

--- lib/data/ApiController.dart
+++ lib/data/ApiController.dart
@@ -10,4 +10,31 @@ class ApiController {
       return null;
     }
   }
+
+  static Future<dynamic> post(String url, Map<String, dynamic> data) async {
+    var response = await http.post(Uri.parse(url), body: data);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
+
+  static Future<dynamic> put(String url, Map<String, dynamic> data) async {
+    var response = await http.put(Uri.parse(url), body: data);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
+
+  static Future<dynamic> delete(String url) async {
+    var response = await http.delete(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
 }
