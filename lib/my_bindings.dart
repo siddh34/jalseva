6b56ff26e86a9deec48f65f34513d2219580fbf4
siddharthsutar <ssiddharthsutar@gmail.com>
2023-09-24 23:01:24 +0530

    added bindings

--- lib/my_bindings.dart
+++ lib/my_bindings.dart
@@ -1,9 +1,19 @@
 import 'package:get/get.dart';
+import 'package:jalseva/screens/captionEditScreen/caption_edit_controller.dart';
+import 'package:jalseva/screens/mainScreen/main_screen_controller.dart';
 import 'package:jalseva/screens/postScreen/post_screen_controller.dart';
+import 'package:jalseva/screens/profileScreen/profile_screen_controller.dart';
 
 class MyBindings implements Bindings {
   @override
   void dependencies() {
     Get.put(PostScreenController());
+
+    Get.put(ProfileScreenController());
+
+    Get.put(MainScreenController());
+
+    Get.put(CaptionEditController());
+
   }
 }
