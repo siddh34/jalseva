0a578cc1308f3948d7ac8c68ed4111f3b4d62b18
siddharthsutar <ssiddharthsutar@gmail.com>
2023-09-24 22:31:15 +0530

    added basic login screen

--- lib/screens/loginScreen/login_screen.dart
+++ lib/screens/loginScreen/login_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'JalShakti',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              const SizedBox(height: 16.0),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // TODO: Implement login logic
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
