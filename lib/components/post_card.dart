ef9946b32948493466f4f503c864ea2cda9a7d23
siddharthsutar <ssiddharthsutar@gmail.com>
2023-10-16 23:57:09 +0530

    fix post screen

--- lib/components/post_card.dart
+++ lib/components/post_card.dart
@@ -8,122 +8,125 @@ class PostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Image.asset(
-                        'assets/male.png',
-                        width: 30,
-                        height: 30,
-                        fit: BoxFit.cover,
-                        ),
-                      const SizedBox(width: 8),
-                      Text.rich(
-                        TextSpan(
-                          text: "Aditya",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/male.png',
+                          width: 30,
+                          height: 30,
+                          fit: BoxFit.cover,
                           ),
-                        ),
-                        ),
-                    ],
+                        const SizedBox(width: 8),
+                        Text.rich(
+                          TextSpan(
+                            text: "Aditya",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                            text: "View on maps",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          ),
+                        const SizedBox(width: 8),
+                        Image.asset(
+                          'assets/google_maps.png',
+                          width: 30,
+                          height: 30,
+                          fit: BoxFit.cover,
+                          ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.network(
+                      post.imageUrl,
+                      width: 300.0,
+                      height: 250.0,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  Row(
-                    children: [
-                      Text.rich(
-                        TextSpan(
-                          text: "View on maps",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        ),
-                      const SizedBox(width: 8),
-                      Image.asset(
-                        'assets/google_maps.png',
-                        width: 30,
-                        height: 30,
-                        fit: BoxFit.cover,
-                        ),
-                    ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  post.title,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(post.description),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.thumb_up),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.comment),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.share),
+                    onPressed: () {},
                   ),
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.network(
-                    post.imageUrl,
-                    width: 300.0,
-                    height: 250.0,
-                    fit: BoxFit.cover,
-                  ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Likes: ${post.likes}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      'Comments: ${post.comments}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      'Shares: ${post.shares}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                post.title,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(post.description),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.thumb_up),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.comment),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.share),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'Likes: ${post.likes}',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Text(
-                    'Comments: ${post.comments}',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Text(
-                    'Shares: ${post.shares}',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
